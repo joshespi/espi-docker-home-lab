d9522daa08a58faea27babae1960917bea6c627a
Espi <joshespi@gmail.com>
2023-05-21 12:31:11 -0600

    Update backup-container.sh

--- backup-container.sh
+++ backup-container.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+set -e
 
 # Define the paths
 espifam_compose_file="/home/joshe/docker/espifam/docker-compose.yml" 
 espifam_directory="/home/joshe/docker/espifam"
 espifam_sql="/home/joshe/docker/espifam/db_data"
+espifam_zip="/home/joshe/docker/espifam.zip"
 
 # Bring down the containers
 docker-compose -f "$espifam_compose_file" down
 
-#if the container has a sql container you'll want to rewrite the permissions to make sure all tables get collected. this also would require sudo prividges
+# Change ownership for SQL container
 sudo chown -R $USER:$USER "$espifam_sql"
 
-
 # Zip the entire directory
-zip -r "$espifam_directory.zip" "$espifam_directory"
+zip -r "$espifam_zip" "$espifam_directory"
 
 # Start the containers back up
 docker-compose -f "$espifam_compose_file" up -d
