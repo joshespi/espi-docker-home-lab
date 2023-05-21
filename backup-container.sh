ac327150d8d23b6a35f41020a66e487d0f579dba
Espi <joshespi@gmail.com>
2023-05-21 12:30:04 -0600

    Update backup-container.sh

--- backup-container.sh
+++ backup-container.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#rsync -av --delete /home/joshe/docker/ joshe@192.168.1.35:/mnt/backup/minecraft
-
 # Define the paths
 espifam_compose_file="/home/joshe/docker/espifam/docker-compose.yml" 
 espifam_directory="/home/joshe/docker/espifam"
+espifam_sql="/home/joshe/docker/espifam/db_data"
+
 # Bring down the containers
 docker-compose -f "$espifam_compose_file" down
+
+#if the container has a sql container you'll want to rewrite the permissions to make sure all tables get collected. this also would require sudo prividges
+sudo chown -R $USER:$USER "$espifam_sql"
+
+
 # Zip the entire directory
 zip -r "$espifam_directory.zip" "$espifam_directory"
+
 # Start the containers back up
 docker-compose -f "$espifam_compose_file" up -d
